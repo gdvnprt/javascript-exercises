b390d0bdd0942979623e1499094ac81de050d79b
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-26 12:31:54 -0700

    Remove pseudocode
    Clean up the code lines

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,4 @@
 const reverseString = function(string) {
-// create an empty array
-// add all characters from the input into the array in reverse order
-// output the array in joined format
     let reverseArray = []
     for (let i = 0; i < string.length; i++) {
         reverseArray.unshift(string.slice(i, i + 1));
