a9923d0342812c5b2b23402a01acb2bae1fff07d
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-26 11:34:49 -0700

    Code potential solution to test
    Draft the function to pass the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+// add string to an empty array
+// repeat this num times
+// output array contents
+let stringArray = []
+for (let i=0; i < num; i++) {
+    stringArray.push(string)
+}
+return stringArray.join()
 };
 
 // Do not edit below this line
