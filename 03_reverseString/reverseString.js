159efc31f226ce1701432a345651bf027405366a
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-26 12:12:52 -0700

    Create function to test
    Code function to attempt to pass test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 // create an empty array
 // add all characters from the input into the array in reverse order
 // output the array in joined format
+let reverseArray = []
+for (i = 0, i < string.length, i++)
+    reverseArray.unshift(string.slice(i, i))
 };
+return  reverseArray.join("")
 
 // Do not edit below this line
 module.exports = reverseString;
