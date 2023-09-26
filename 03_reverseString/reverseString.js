1152de5bd5d7d206c6bee2a9c92a0b8c65375baf
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-26 12:30:24 -0700

    Debug and ready for additional tests
    Adjust code to pass first test and enable additional tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,11 +2,12 @@ const reverseString = function(string) {
 // create an empty array
 // add all characters from the input into the array in reverse order
 // output the array in joined format
-let reverseArray = []
-for (i = 0, i < string.length, i++)
-    reverseArray.unshift(string.slice(i, i))
-};
-return  reverseArray.join("")
+    let reverseArray = []
+    for (let i = 0; i < string.length; i++) {
+        reverseArray.unshift(string.slice(i, i + 1));
+    }
+    return  reverseArray.join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
