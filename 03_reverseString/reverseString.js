7de46db2d724818d7ce88cb7f6fb65bff3b51175
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-26 11:56:36 -0700

    Add pseudocode
    Explain what needs to happen

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
 const reverseString = function() {
-
+// create an empty array
+// add all characters from the input into the array in reverse order
+// output the array in joined format
 };
 
 // Do not edit below this line
