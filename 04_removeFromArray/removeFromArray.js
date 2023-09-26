ebd76c65e948d461a077c343789b01eeed8feeb4
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-26 13:48:01 -0700

    Create basic function
    Code a function that removes one specific value from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove) {
+    // find the index # of the values in the array to be removed
+    let removeIndex = array.indexOf(remove);
+    // return a new array with those index numbers removed
+    array.splice(removeIndex, 1);
+    return array;
+    // loop function for each additional argument
 };
 
 // Do not edit below this line
