1ffa825df568253a200cdab0f91226636390ecfc
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-27 10:12:00 -0700

    Make pseudocode and base loop
    Write out what I want to be done and start the base loop for the function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let newSum = 0
+    for (let i = a; i < b; i++) { // create a loop starting at a
+    // create a new number variable starting at a
+    // add each number from a to b in it
+    // return the sum of all
 };
 
 // Do not edit below this line
