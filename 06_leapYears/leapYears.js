4be7ec82a6a694ea479821d0dfffb59ef1059358
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-27 11:20:53 -0700

    Debug function and enable additional tests
    Remove quotes from true/false returns and remove skips from additional tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,12 @@
 const leapYears = function(year) {
     if (year % 400 === 0) {
-        return "true";
+        return true;
     } else if (year % 100 === 0) {
-        return "false";
+        return false;
     } else if (year % 4 === 0) {
-        return "true";
+        return true;
     } else {
-        return "false";
+        return false;
     };
 };
 
