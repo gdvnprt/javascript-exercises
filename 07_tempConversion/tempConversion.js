f7405bbde1edefb50f66aa0401f60f9fdf1a2b1c
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-27 11:44:25 -0700

    Debug function and enable additional tests
    Function returns number and remove test skips

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const convertToCelsius = function(fahrenheit) {
   // x °F ≘ (x − 32) × 5/9 °C 
   const toCelsius = (fahrenheit - 32) * 5 / 9;
-  return toCelsius.toFixed(1);
+  return Number(toCelsius.toFixed(1));
 };
 
 const convertToFahrenheit = function(celsius) {
   // x °C ≘ (x × 9/5 + 32) °F 
   const toFahrenheit = (celsius * 9 / 5) + 32;
-  return toFahrenheit.toFixed(1);
+  return Number(toFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
