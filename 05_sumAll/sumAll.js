4750dfec44f11f2e0f3d657ea5fe94637fa523fe
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-27 10:59:56 -0700

    Add conditions for numbers
    code if else function to pass tests of non-number values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,17 @@
 const sumAll = function(a, b) {
-    let newSum = 0
-    let sortNumbers = [a, b]
-    sortNumbers.sort(function(x, y){return x - y});
-    const lowNumber = sortNumbers[0];
-    const highNumber = sortNumbers[1];
-    for (let i = lowNumber; i < highNumber + 1; i++) { // create a loop starting at a
-        newSum += i;
+    if (typeof a === "number" && typeof b === "number") {
+        let newSum = 0;
+        let sortNumbers = [a, b];
+        sortNumbers.sort(function(x, y){return x - y});
+        const lowNumber = sortNumbers[0];
+        const highNumber = sortNumbers[1];
+        for (let i = lowNumber; i < highNumber + 1; i++) { 
+            newSum += i;
+        };
+        return newSum;
+    } else {
+        return "ERROR"
     };
-    return newSum;
-    // create a new number variable starting at a
-    // add each number from a to b in it
-    // return the sum of all
 };
 
 // Do not edit below this line
