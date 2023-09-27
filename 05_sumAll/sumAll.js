a5ac817d6bbadfb51bf949adf16684caa39b0d33
Gavin Davenport <gavinkdavenport@gmail.com>
2023-09-27 10:50:23 -0700

    Debug code
    Debug function to pass first three tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(a, b) {
     let newSum = 0
     let sortNumbers = [a, b]
-    sumNumbers.sort(function(x, y){return x - y});
+    sortNumbers.sort(function(x, y){return x - y});
     const lowNumber = sortNumbers[0];
     const highNumber = sortNumbers[1];
     for (let i = lowNumber; i < highNumber + 1; i++) { // create a loop starting at a
