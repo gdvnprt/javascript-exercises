e17f95b9c3f8773227f423f8cba9d61a8f9bb075
Gavin Davenport <gavinkdavenport@gmail.com>
2024-01-04 13:05:06 -0800

    Solve first test
    Code function to pass first test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let letters = a.split('');
+    let palin = letters.reverse();
+    if (palin === letters) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
