4dd596813edb2adf13d966f75b808c221db5866b
Gavin Davenport <gavinkdavenport@gmail.com>
2024-01-05 14:12:24 -0800

    Draft solution function
    code function to attempt to solve tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a <= 0) {
+        return "OOPS";
+    } else {
+        let arr = [1, 1, 2, 3];
+        for (var i = 3; i < (a - 1); i++) {
+            arr.push(arr[i - 2] + arr[i - 1]);
+        };
+        return arr[a - 1];
+    };
 };
 
 // Do not edit below this line
