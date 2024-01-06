b464fb760ec8779440b41f8b6fef0063776bb45e
Gavin Davenport <gavinkdavenport@gmail.com>
2024-01-05 20:16:19 -0800

    Debug function
    adjust code to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,15 +1,17 @@
 const findTheOldest = function(i) {
+    const date = new Date();
+    const year = date.getFullYear();
     i.sort(function(a, b) {
         if ("yearOfDeath" in a) {
-        const lastguy = a.yearOfDeath - a.yearOfBirth;
+            lastguy = a.yearOfDeath - a.yearOfBirth;
         } else {
-        const lastguy = getFullYear() - a.yearOfBirth;
+            lastguy = year - a.yearOfBirth;
         };
         if ("yearOfDeath" in b) {
-            const lastguy = b.yearOfDeath - b.yearOfBirth;
-            } else {
-            const lastguy = getFullYear() - b.yearOfBirth;
-            };
+            nextguy = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            nextguy = year - b.yearOfBirth;
+        };
         return lastguy > nextguy ? -1 : 1;
     });
     return i[0];
