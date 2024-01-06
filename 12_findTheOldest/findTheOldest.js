267dc40b2390f005ba0ec20eda516c27bbb9423a
Gavin Davenport <gavinkdavenport@gmail.com>
2024-01-05 19:44:32 -0800

    Draft first function
    code baseline function to return oldest. will need to update for no death date

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(i) {
+    people.sort(function(a, b) {
+        const lastguy = a.yearOfDeath - a.yearOfBirth;
+        const nextguy = b.yearOfDeath - b.yearOfBirth;
+        return lastguy > nextguy ? -1 : 1;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
