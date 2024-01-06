c8247df7e614c6dd78e15b16a87001ff57986082
Gavin Davenport <gavinkdavenport@gmail.com>
2024-01-05 20:04:24 -0800

    Draft function for final tests
    add functionality for finding the oldest with no death year

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,15 @@
 const findTheOldest = function(i) {
     i.sort(function(a, b) {
+        if ("yearOfDeath" in a) {
         const lastguy = a.yearOfDeath - a.yearOfBirth;
-        const nextguy = b.yearOfDeath - b.yearOfBirth;
+        } else {
+        const lastguy = getFullYear() - a.yearOfBirth;
+        };
+        if ("yearOfDeath" in b) {
+            const lastguy = b.yearOfDeath - b.yearOfBirth;
+            } else {
+            const lastguy = getFullYear() - b.yearOfBirth;
+            };
         return lastguy > nextguy ? -1 : 1;
     });
     return i[0];
