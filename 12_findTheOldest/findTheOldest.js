8c0461b619fc022e3719d3f1d7b3510fd2e05e43
Gavin Davenport <gavinkdavenport@gmail.com>
2024-01-05 19:46:59 -0800

    Debug first draft
    pass first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,10 @@
 const findTheOldest = function(i) {
-    people.sort(function(a, b) {
+    i.sort(function(a, b) {
         const lastguy = a.yearOfDeath - a.yearOfBirth;
         const nextguy = b.yearOfDeath - b.yearOfBirth;
         return lastguy > nextguy ? -1 : 1;
     });
-    return people[0];
+    return i[0];
 };
 
 // Do not edit below this line
